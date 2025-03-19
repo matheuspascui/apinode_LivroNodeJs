a8978033b5e13c39ab29e4bd0974fe5b797a9eaf
Matheus Páscui <matheuspascui@gmail.com>
2025-03-19 15:41:00 -0300

    taking out the os.fork() process because nodemon crashes. In pure node calling it works, commenting just to have the previous version available

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 import app from '../app.js'
 import debug from 'debug'
+import cluster from 'cluster'
+import os from 'os'
+
+// const cpus = os.cpus();
+// console.log('cpus returned from the function: ', cpus);
 
 const log = debug('apinode:www')
+// log(`Information returned by cpus: ${cpus}`);
+// log(`Type of cpus variable: ${typeof cpus}`);
+
+// if (cluster.isPrimary) {
+//     cpus.foreach(_ => cluster.fork())
+//     cluster.on('exit', (err) => log(err))
+// } else {
+//     app.listen(3000, log('server started on port 3000...'))
+// }
+
+
 
 app.listen(3000, () => log('server started on port 3000...'));
